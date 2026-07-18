83d108fc4e26ed45a28d579fe4647952365dbb17
Tanul <tanulgupta123@gmail.com>
2026-07-19 00:53:31 +0200

    Add: longest-consecutive-sequence - submission-8

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-8.py
@@ -0,0 +1,58 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        # # 1. Brute Force Solution.
+        # res = 0
+        # setnums = set(nums)
+
+        # for num in nums:
+        #     streak, curr = 0, num
+        #     while curr in setnums:
+        #         streak += 1
+        #         curr += 1
+
+        #     res = max(res, streak)
+
+        # return res
+
+        # 2. Solution using Sorting.
+        if not nums:
+            return 0
+
+        res = 1
+
+        nums.sort() # Sorting the list of numbers. 
+        n = len(nums)
+        curr, streak = nums[0], 1
+
+        for i in range(1,n):
+            if nums[i]==curr:
+                continue
+            if nums[i]==curr+1:
+                streak += 1
+            else:
+                streak = 1
+            
+            res = max(res, streak)
+            curr = nums[i]
+
+        return res
+
+        
+        # # 3. Use of Hash Set
+        # setnums = set(nums)
+        # longest = 0
+        # for num in setnums:
+        #     if (num-1) not in setnums:
+        #         length = 1
+        #         while (num+length) in setnums:
+        #             length += 1
+
+        #         longest = max(longest, length)
+
+        # return longest
+
+        # 4. Use of Hash Map
+
+
+
+
