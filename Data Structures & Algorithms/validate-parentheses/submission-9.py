7c9a51f706b63df661884a005e7a0d790152c00b
Tanul <tanulgupta123@gmail.com>
2026-08-04 12:18:21 +0200

    Add: validate-parentheses - submission-9

---
+++ Data Structures & Algorithms/validate-parentheses/submission-9.py
@@ -0,0 +1,82 @@
+class Solution:
+
+    def isValid(self, s:str) -> bool:
+        OPENING_BRACKETS = {"(","{","["}
+        lenstring = len(s)
+        stack = []
+        pairs = {
+            ")" : "(",
+            "]" : "[",
+            "}" : "{"
+        }
+
+
+        for (i, char) in enumerate(s):
+            if char in OPENING_BRACKETS:
+                stack.append(char)
+            elif stack and stack[-1]==pairs[char]:
+                stack.pop()
+            else: 
+                return False
+
+        return (not stack)
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # def isValid(self, s: str) -> bool:
+    #     stack = []
+    #     pairs = {
+    #         ")" : "(",
+    #         "]" : "[",
+    #         "}" : "{" 
+    #     }
+    #     lenstring = len(s)
+    #     if lenstring % 2 == 1:
+    #         return False
+        
+    #     for c in s:
+    #         if c in "([{":
+    #             stack.append(c)
+    #         elif stack and stack[-1]== pairs[c]:
+    #             stack.pop()
+    #         else:
+    #             return False
+
+    #     return len(stack) == 0
+
+
+
