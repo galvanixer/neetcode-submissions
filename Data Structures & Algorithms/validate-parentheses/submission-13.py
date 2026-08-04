cb17922247c50f9ff2a842078a7e685adc7a699e
Tanul <tanulgupta123@gmail.com>
2026-08-04 12:20:12 +0200

    Add: validate-parentheses - submission-13

---
+++ Data Structures & Algorithms/validate-parentheses/submission-13.py
@@ -0,0 +1,84 @@
+class Solution:
+
+    def isValid(self, s:str) -> bool:
+        OPENING_BRACKETS = {"(","{","["}
+        lenstring = len(s)
+        stack = []
+        pairs = {
+            ")" : "(",
+            "]" : "[",
+            "}" : "{"
+        }
+
+        if lenstring%2==1:
+            return False
+
+        for char in s:
+            if char in OPENING_BRACKETS:
+                stack.append(char)
+            elif len(stack)>0 and stack[-1]==pairs[char]:
+                stack.pop()
+            else: 
+                return False
+
+        return (not stack)
+
+    # def isValid(self, s: str) -> bool:
+    #     stack = []
+    #     pairs = {
+    #         ")" : "(",
+    #         "]" : "[",
+    #         "}" : "{" 
+    #     }
+    #     lenstring = len(s)
+    #     if lenstring % 2 == 1:
+    #         return False
+        
+    #     for c in s:
+    #         if c in "([{":
+    #             stack.append(c)
+    #         elif stack and stack[-1]== pairs[c]:
+    #             stack.pop()
+    #         else:
+    #             return False
+
+    #     return len(stack) == 0      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
