a905eae9c778659ba561fa37d3d341432176d8cb
Tanul <tanulgupta123@gmail.com>
2026-08-06 01:04:14 +0200

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.py
@@ -0,0 +1,35 @@
+class Solution:
+    # > Optimal Solution (Closed Interval): time: O(logn), space: O(1)
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        left, right = 0, n-1
+        searchidx = -1
+
+        while left<=right:
+            mid = left + (right-left)//2 # So that in languages like C it does not overflow.
+            if nums[mid] == target:
+                searchidx = mid
+                break
+            elif nums[mid] > target:
+                right = mid - 1
+            elif nums[mid] < target:
+                left = mid + 1
+
+        return searchidx
+
+    # > Optimal Solution (Half-open [l,r)): Time: O(logn), Space: O(1)
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        l, r = 0, n # Left and right
+
+        while l < r:
+            mid = l + (r-l)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]>target:
+                r = mid
+            else:
+                l = mid + 1
+
+        return -1
+        
