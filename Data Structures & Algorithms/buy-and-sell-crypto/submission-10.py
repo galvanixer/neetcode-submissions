3bd7206d2f459ef5877f4c244cdade42394c0be2
Tanul <tanulgupta123@gmail.com>
2026-08-10 14:02:18 +0200

    Add: buy-and-sell-crypto - submission-10

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-10.py
@@ -0,0 +1,40 @@
+class Solution:
+    # > Brute Force Solution: time: O(n^2), space: O(1)
+    def maxProfit(self, prices: List[int]) -> int:
+
+        nprices = len(prices)
+        maxprofit = 0
+        
+        for i in range(nprices):
+            for j in range(i+1, nprices):
+                maxprofit = max(maxprofit, prices[j]-prices[i])
+
+        return maxprofit
+
+    def maxProfit(self, prices: List[int]) -> int:
+
+        nprices = len(prices)
+        l, r = 0, 1
+        maxP = 0
+
+        while r < nprices:
+            if prices[r] > prices[l]:
+                maxP = max(maxP, prices[r]-prices[l])
+            else:
+                l = r
+            r += 1
+
+        return maxP
+
+    def maxProfit(self, prices: List[int]) -> int:
+        maxP = 0
+        minbuy = prices[0]
+
+        for sp in prices:
+            profit = sp - minbuy
+            maxP = max(maxP, profit)
+            minbuy = min(minbuy, sp)
+
+        return maxP
+
+        
